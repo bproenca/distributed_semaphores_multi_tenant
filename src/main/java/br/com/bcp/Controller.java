a921e692f737099927643f5ae18265b3ddaf3c98
Bruno Proenca <bruno.proenca@synchro.com.br>
2024-10-15 14:26:23 -0300

    backpressure

--- src/main/java/br/com/bcp/Controller.java
+++ src/main/java/br/com/bcp/Controller.java
@@ -1,5 +1,7 @@
 package br.com.bcp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,8 @@ public class Controller {
     @Autowired
     private Admin admin;
 
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @PostMapping("/consume")
     public String consume(
         @RequestParam(value = "threads", defaultValue = "2") Integer threads,
@@ -33,31 +37,40 @@ public class Controller {
     public String produce() {
         int cnt = 1;
         for (Tenant t : Tenant.values()) {
+            logger.info("Producing {} messages in queue {}", t.getNumberOfMessages(), t.getQueueName());
             for (int i = 0; i < t.getNumberOfMessages(); i++) {
                 producer.produce(t.getQueueName(), "Message number: " + cnt++);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
+            sleep(100);
         }
         return "produce ok";
     }
 
 	@PostMapping("/config")
     public String config() {
-
         for (Tenant t : Tenant.values()) {
             admin.purge(t.getResourceLock());
-        	producer.produce(t.getResourceLock(), t.getQueueName());
+            logger.info("Configuring lock queues {} with backpressure {} ", t.getResourceLock(), t.getBackPressure());
+            for (int i = 0; i < t.getBackPressure(); i++) {
+                producer.produce(t.getResourceLock(), t.getQueueName());
+            }
+            sleep(100);
         }
 		return "config ok";
     }
 
     @GetMapping("/ping")
     public String ping() {
+        logger.info("pong");
         return "pong";
+    }
+
+    private void sleep(int time) {
+         try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            logger.error("Sleep error", e);
+        }
     }
 
 }
